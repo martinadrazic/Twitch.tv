e8c3e010f45822e3c5291f0e26208ced57835ea1
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-22 16:24:50 +0200

    version 5

--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@ var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominat
 $.getJSON("https://api.twitch.tv/kraken/streams?channel=freecodecamp,admiralbulldog,thijshs,datto,iwilldominate,p4wnyhof,rush&client_id=qmrzfjzinqtouj42cgd3sfysr5kctvi", function(json) {
 	console.log(json);
 
-	compare(stream, json);
+	window.onload = function() {
+    document.getElementById("stream").innerHTML = compare(stream, json);        
+	}
+	// compare(stream, json);
 });
 
-
 function compare(stream, json) {
-		for (var i = 0; i < stream.length; i++) {
-			for (var a = 0; a < json.length; a++) {
-				if (stream[i] === json.streams[a].channel.name) {
-					console.log(json.streams[a].channel.name);
-				} else {
-					console.log(stream[i]);
-				}
+	for (var i = 0; i < stream.length; i++) {
+		for (var a = 0; a < json.streams.length; a++) {
+			if (stream[i] === json.streams[a].channel.name) {
+				console.log(json.streams[a].channel.name);
+			} else {
+				console.log(stream[i] + " is offline");
 			}
 		}
 	}
+}
+
