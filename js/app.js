40b309081fe02ff2485d53f4fc37f97d7988e8e9
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-22 14:07:25 +0200

    version 2

--- js/app.js
+++ js/app.js
@@ -1,18 +1,10 @@
-
-var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominate", "p4wnyhof", "datto"];
 var content = "";
 
-for (var i = 0; i < stream.length; i++) {
-	$.getJSON("https://api.twitch.tv/kraken/streams?channel=" + stream[i] + "&client_id=qmrzfjzinqtouj42cgd3sfysr5kctvi", function(json) {
-		console.log(json);
+$.getJSON("https://api.twitch.tv/kraken/streams?channel=freecodecamp,admiralbulldog,thijshs,datto,iwilldominate,p4wnyhof,rush&client_id=qmrzfjzinqtouj42cgd3sfysr5kctvi", function(json) {
+	console.log(json);
 
-		// if (json.streams[0].channel.display_name === "") {
+	// if (json.streams[0].channel.display_name === "") {
 
-		// }
+	// }
 
-	});
-
-
-}
-
-	
+});
