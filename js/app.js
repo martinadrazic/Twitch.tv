bd019aa9c145ff0098f3030c3cf5bed50bad388d
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-26 16:18:34 +0200

    final touch :)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var content = "";
-var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominate", "p4wnyhof", "datto", "capcomfighters", "quin69", "massansc", "brunofin","comster404"];
+var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominate", "p4wnyhof", "datto", "capcomfighters", "quin69", "massansc", "brunofin", "comster404"];
 var streamString = stream.toString();
 var res = {};
 var container = document.getElementById("stream");
@@ -32,34 +32,33 @@ function drawRow(name, stream) {
 	if (stream) {
 		var content =
 			'<div class="row streamers">' +
-				'<div class="col-md-4"><a href=" ' + stream.channel.url + ' " target="_blank" class="stream-name">' + name + '</a></div>' +
-				'<div class="col-md-4"> ' +
-					'<p class="status-online">Online</p>' +
-				'</div>' +
-				'<div class="col-md-4 dropdown">' +
-					'<p onclick="myFunction(\'' + stream.channel.display_name + '\')" class="drop">Details</p>' +
-					'<div id="myDropdown-' + stream.channel.display_name + '" class="dropdown-content">' +
-						'<p>Display name:</p><span> ' + stream.channel.display_name + '</span>' +
-						'<p>Game:</p><span> ' + stream.channel.game + ' </span>' +
-						'<p>Followers:</p><span> ' + stream.channel.followers + ' </span>' +
-						'<p>Language:</p><span> ' + stream.channel.language + ' </span>' +
-						'<p>Views:</p><span> ' + stream.channel.views + ' </span>' +
-					'</div>' +
-				'</div>' +
+			'<div class="col-md-4"><a href=" ' + stream.channel.url + ' " target="_blank" class="stream-name">' + name + '</a></div>' +
+			'<div class="col-md-4"> ' +
+			'<p class="status">Online</p>' +
 			'</div>' +
-			'<div class="delimiter-horizontal"></div>' 
+			'<div class="col-md-4 dropdown">' +
+			'<p onclick="myFunction(\'' + stream.channel.display_name + '\')" class="drop">Details</p>' +
+			'<div id="myDropdown-' + stream.channel.display_name + '" class="dropdown-content">' +
+			'<p>Display name:</p><span> ' + stream.channel.display_name + '</span>' +
+			'<p>Game:</p><span> ' + stream.channel.game + ' </span>' +
+			'<p>Followers:</p><span> ' + stream.channel.followers + ' </span>' +
+			'<p>Views:</p><span> ' + stream.channel.views + ' </span>' +
+			'</div>' +
+			'</div>' +
+			'</div>' +
+			'<div class="delimiter-horizontal"></div>'
 	} else {
 		var content =
 			'<div class="row streamers">' +
-				'<div class="col-md-4">' +
-					'<a href="https://www.twitch.tv/'+ name +'" target="_blank" class="stream-name">' + name + '</a>' +
-				'</div>' +
-				'<div class="col-md-4">' +
-					'<p class="status-offline">Offline</p>' +
-				'</div>' +
-				'<div class="col-md-4">' +
-					'<p class="activity">Not available</p>' +
-				'</div>' +
+			'<div class="col-md-4">' +
+			'<a href="https://www.twitch.tv/' + name + '" target="_blank" class="stream-name">' + name + '</a>' +
+			'</div>' +
+			'<div class="col-md-4">' +
+			'<p class="status">Offline</p>' +
+			'</div>' +
+			'<div class="col-md-4">' +
+			'<p class="activity">Not available</p>' +
+			'</div>' +
 			'</div>' +
 			'<div class="delimiter-horizontal"></div>'
 	}
