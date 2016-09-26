d1848c1e6c5ef2355f31e968d9c700c576e19411
martinadrazic <dr.martinadrazic@gmail.com>
2016-09-26 15:14:58 +0200

    version 8

--- js/app.js
+++ js/app.js
@@ -1,36 +1,23 @@
 var content = "";
-var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominate", "p4wnyhof", "datto"];
+var stream = ["freecodecamp", "rush", "thijshs", "admiralbulldog", "iwilldominate", "p4wnyhof", "datto", "capcomfighters", "quin69", "massansc", "brunofin","comster404"];
 var streamString = stream.toString();
 var res = {};
 var container = document.getElementById("stream");
 
 $.getJSON("https://api.twitch.tv/kraken/streams?channel=" + streamString + "&client_id=qmrzfjzinqtouj42cgd3sfysr5kctvi", function(json) {
-	// console.log(json);
+	console.log(json);
 	compare(stream, json);
 });
 
 
-
 function compare(stream, json) {
-	// var online;
-	// var offline;
-
 	for (var i = 0; i < stream.length; i++) {
 		res[stream[i]] = getStream(json.streams, stream[i]);
 		drawRow(stream[i], res[stream[i]]);
-
-		// for (var a = 0; a < json.streams.length; a++) {
-		// 	if (stream[i] === json.streams[a].channel.name) {
-		// 		offline += json.streams[a].channel.name;
-		// 		// console.log(online);
-		// 	} else {
-		// 		offline += stream[i];
-		// 		// console.log(offline + " is offline");
-		// 	}
-		// }
 	}
 	console.log(res);
 }
+
 
 function getStream(list, name) {
 	for (var i = 0; i < list.length; i++) {
@@ -42,33 +29,50 @@ function getStream(list, name) {
 
 
 function drawRow(name, stream) {
-	// var content = "<div>" + name + "</div>";
-
 	if (stream) {
 		var content =
-			'<div><a href=" ' + stream.channel.url + ' " target="_blanck">' + name + '</a> Online</div>' +
-			'<div class="dropdown">' +
-			'<p onclick="myFunction()" class="dropbtn">Details</p>' +
-			'<div id="myDropdown" class="dropdown-content">' +
-			'<p>Display name: ' + stream.channel.display_name + ' </p>' +
-			'<p>Game: ' + stream.channel.game + ' </p>' +
-			'<p>Followers: ' + stream.channel.followers + ' </p>' +
-			'<p>Language: ' + stream.channel.language + ' </p>' +
-			'<p>Views: ' + stream.channel.views + ' </p>' +
+			'<div class="row streamers">' +
+				'<div class="col-md-4"><a href=" ' + stream.channel.url + ' " target="_blank" class="stream-name">' + name + '</a></div>' +
+				'<div class="col-md-4"> ' +
+					'<p class="status">Online</p>' +
+				'</div>' +
+				'<div class="col-md-4 dropdown">' +
+					'<p onclick="myFunction(\'' + stream.channel.display_name + '\')" class="drop">Details</p>' +
+					'<div id="myDropdown-' + stream.channel.display_name + '" class="dropdown-content">' +
+						'<p>Display name:</p><span> ' + stream.channel.display_name + '</span>' +
+						'<p>Game:</p><span> ' + stream.channel.game + ' </span>' +
+						'<p>Followers:</p><span> ' + stream.channel.followers + ' </span>' +
+						'<p>Language:</p><span> ' + stream.channel.language + ' </span>' +
+						'<p>Views:</p><span> ' + stream.channel.views + ' </span>' +
+					'</div>' +
+				'</div>' +
 			'</div>' +
-			'</div>' 
+			'<div class="delimiter-horizontal"></div>' 
 	} else {
-		var content = '<div>' + name + ' Offline</div>'
+		var content =
+			'<div class="row streamers">' +
+				'<div class="col-md-4">' +
+					'<a href="https://www.twitch.tv/'+ name +'" target="_blank" class="stream-name">' + name + '</a>' +
+				'</div>' +
+				'<div class="col-md-4">' +
+					'<p class="status">Offline</p>' +
+				'</div>' +
+				'<div class="col-md-4">' +
+					'<p class="activiti">Undefined</p>' +
+				'</div>' +
+			'</div>' +
+			'<div class="delimiter-horizontal"></div>'
 	}
 	container.innerHTML += content;
 }
 
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
+
+function myFunction(name) {
+	document.getElementById("myDropdown-" + name).classList.toggle("show");
 }
 
 window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
+	if (!event.target.matches('.drop')) {
 
 		var dropdowns = document.getElementsByClassName("dropdown-content");
 		var i;
